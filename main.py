cfb85337c2c030a852601606e04134d39d5c0c25
barabulka <jds1032@mail.ru>
2022-10-19 16:22:19 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    history=[['+'],['-'],['/'],['*']]
+    while True:
+        a = int(input())
+        b = int(input())
+        operation = input()
+        if operation=="+":
+            c=f'{a}+{b}={a+b}'
+            print(c)
+            history[0].append(c)
+        elif operation=="-":
+            c=f'{a}-{b}={a-b}'
+            print(c)
+            history[1].append(c)
+        elif operation=="*":
+            c=f'{a}*{b}={a*b}'
+            print(c)
+            history[3].append(c)
+        elif operation=="/":
+            c=f'{a}/{b}={a/b}'
+            print(c)
+            history[2].append(c)
+        else:
+            print("Incorrect enter")
+        for j in range(len(history)):
+            print(history[j][0],history[j][1:])
